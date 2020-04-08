5cadd8aa022e3a94ef08f3a3fa142aeba6351913
kathryn0908 <kathrynlael@gmail.com>
2020-04-08 11:41:32 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,10 @@ require 'pry'
 
 # require_relative '../lib/concerns/findable'
 # require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/paramable'
 
+require_relative '../lib/ClassModule.rb'
+require_relative '../lib/findable.rb'
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
